c408a1e560c31e10aaf29b07ff4cce9199b75e80
GMT-Mati <75912725+GMT-Mati@users.noreply.github.com>
2024-06-25 21:11:03 +0200

    data loading

---
+++ scripts/data_loading.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def load_data():
+    matches = pd.read_csv('../data/WorldCupMatches.csv')
+    players = pd.read_csv('../data/WorldCupPlayers.csv')
+    cups = pd.read_csv('../data/WorldCups.csv')
+    return matches, players, cups
